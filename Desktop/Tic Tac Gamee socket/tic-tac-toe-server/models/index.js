e7dc6e93fb137432fabaaec56b4c3921223c3898
Mukundwa-Aurore <153071111+Mukundwa-Aurore@users.noreply.github.com>
2025-06-13 10:33:48 +0200

    Delete Desktop directory

--- Desktop/Tic Tac Gamee socket/tic-tac-toe-server/models/index.js
+++
@@ -1,9 +0,0 @@
-
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize("tic_tac_toe", "postgres", " ", {
-  host: "localhost",
-  dialect: "postgres",
-});
-
-module.exports = sequelize;
